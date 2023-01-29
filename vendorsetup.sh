62b8242f207d87bb9adce780864f67b50c14ba70
COSMIC <93328579+rayavanindra@users.noreply.github.com>
2023-01-29 12:31:38 +0530

    miatoll: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Xtended-Devices/device_xiaomi_sm6250-common -b xt device/xiaomi/sm6250-common
+git clone https://github.com/Xtended-Devices/vendor_xiaomi_miatoll -b xt vendor/xiaomi/miatoll
+git clone https://github.com/Xtended-Devices/kernel_xiaomi_sm6250 -b xt kernel/xiaomi/sm6250
+git clone https://github.com/Xtended-Devices/vendor_xiaomi_sm6250-common -b xt vendor/xiaomi/sm6250-common
+git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-proton
