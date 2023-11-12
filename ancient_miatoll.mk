33a93b69498337d28db104e96c16c69523035a9b
RahulGorai0202 <rahulgorai0206@gmail.com>
2023-11-12 19:11:53 +0530

    miatoll: Initialized for AncientOS

--- ancient_miatoll.mk
+++ ancient_miatoll.mk
@@ -16,12 +16,9 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
-# Bootanimation Resolution
-TARGET_BOOT_ANIMATION_RES := 1080
-
-PRODUCT_NAME := xtended_miatoll
+PRODUCT_NAME := ancient_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := SM6250
@@ -32,18 +29,13 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 TARGET_SHIPS_GALLERY := false
 
-# Xtended Stuffs
-XTENDED_BUILD_TYPE := OFFICIAL
-XTENDED_BUILD_MAINTAINER := COSMIC
-PROCESSOR_MODEL := atoll
-WITH_GAPPS := true
-TARGET_USES_BLUR := true
-XTENDED_BUILD_DONATE_URL := https://paypal.me/avanindra127
-XTENDED_BUILD_SUPPORT_URL := https://t.me/itz_miatoll_zone404
-
 # Inherit some common device props
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official
+ANCIENT_OFFICIAL := true
+ANCIENT_GAPPS := true
+TARGET_USES_BLUR := false
+PIXEL_STUFF := true
